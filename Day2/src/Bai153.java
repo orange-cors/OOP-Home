bf4d4f049df8feb53487c53dcf7d731437ab481c
orange-cors <mango.cors@gmail.com>
2025-09-19 16:46:02 +0700

    Update

--- Day2/src/Bai153.java
+++ Day2/src/Bai153.java
@@ -6,6 +6,7 @@ public class Bai153 {
         int x = sc.nextInt();
         int total = 10;
         total += x;
+        System.out.println("Total la: " + total);
     }
 }
 //Giảm x đi 1 đơn vị, sau đó gán cho biến total giá trị là hiệu của total và x. Chỉ dùng một lệnh
